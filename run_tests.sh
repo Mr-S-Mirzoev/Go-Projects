1c4ffe4998fa4e6b5e444949ba5ee41dc2807dce
Anton Sulaev <a.sulaev@corp.mail.ru>
2021-10-30 13:29:29 +0300

    lection4: fix pipeline

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,5 @@
 
 set -exuo pipefail
 
-arr=$(find . | ggrep -P '99_hw$' | grep -v '4/99_hw$')
+arr=$(find . | grep -P '99_hw$' | grep -v '4/99_hw$')
 for i in $arr; do golangci-lint -c .golangci.yml run $i/...;done
