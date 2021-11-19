cd4932eab2f55395136aad5cf4a7093a50f84d17
Anton Sulaev <a.sulaev@corp.mail.ru>
2021-11-19 21:48:51 +0300

    lecture 8

---
+++ 8/4_grpc/server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"gitlab.com/mailru-go/lectures-2021-2/8/4_grpc/session"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		log.Fatalln("cant listet port", err)
+	}
+
+	server := grpc.NewServer()
+
+	session.RegisterAuthCheckerServer(server, NewSessionManager())
+
+	fmt.Println("starting server at :8081")
+	server.Serve(lis)
+}
